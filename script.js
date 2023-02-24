bf622e6ed9bf40357e895a74e2f3a8846f593cc3
zilviazz <zilvia524@gmail.com>
2023-02-24 08:47:21 +0700

    memberikan komen

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// membuat variabel dan memperoleh elemen dengan Queryselector
 const menu = document.querySelector('#menu-bars');
 const header = document.querySelector('header');
 
+// untuk menampilkan header saat menu bar di klik
 menu.addEventListener('click', function(){
     menu.classList.toggle('fa-times');
     header.classList.toggle('active');
